ce185d6ffe17148366f43573c5223016c9193d06
uriy <itemruby@gmail.com>
2016-11-22 22:48:00 +0300

    put patch delete

--- Router/bookRouter.js
+++ Router/bookRouter.js
@@ -24,17 +24,62 @@ var router = function () {
             })
         });
 
-    bookRouter.route('/Book/:bookId').get(function (req, res) {
-        console.log(req.params.bookId);
+    bookRouter.use('/Book/:bookId', function (req, res, next) {
         Book.findById(req.params.bookId, function (err, book) {
             if(err){
                 res.status(500).send(err);
+            } else if(book){
+                req.book = book;
+                next();
             } else {
-                res.json(book)
+                res.status(404).send('нельзя найти книгу');
             }
 
         })
     });
+
+    bookRouter.route('/Book/:bookId').get(function (req, res) {
+          res.json(req.book);
+
+    }).put(function (req, res) {
+
+                    req.book.title = req.body.title;
+                    req.book.author = req.body.author;
+                    req.book.genre = req.body.genre;
+                    req.book.read = req.body.read;
+                    req.book.save(function (err) {
+                        if(err){
+                            res.status(500).send(err);
+                        } else {
+                            res.json(req.book);
+                        }
+                    });
+                    res.json(req.book);
+        })
+        .patch(function (req, res) {
+            if(req.body._id){
+                delete req.body._id;
+                for(var p in req.body){
+                    req.book[p] = req.body[p];
+                }
+                req.book.save(function (err) {
+                    if(err){
+                        res.status(500).send(err);
+                    } else {
+                        res.json(req.book);
+                    }
+                })
+            }
+        })
+        .delete(function (req, res) {
+            req.book.remove(function (err) {
+                if(err){
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Удалена');
+                }
+            })
+        });
     
     return bookRouter;
 };
